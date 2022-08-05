aacb40d8beb3121b1fe006032050c3903d85f58c
Tierney Cyren <hello@bnb.im>
2022-08-05 15:27:22 -0400

    chore: rename variable for consistency
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- helpers/database/fetchDataForCountryCode.js
+++ helpers/database/fetchDataForCountryCode.js
@@ -6,14 +6,14 @@ const validateCountryCode = require('../data/validateCountryCode')
 
 // this will probably go in an index.js fastify route
 async function fetchDataForCountryCode (countryCode) {
-  const cleanCountryCode = await validateCountryCode(countryCode, 'code')
+  const validated = await validateCountryCode(countryCode, 'code')
   // spin up the database connection
   const connection = await mysql.createConnection(database)
 
   // query as a variable so we can cleanly keep an eye on it
   const query = 'SELECT * FROM `countries` WHERE `number` = ?' // note that the `?` will be replaced with information passed to connection.execute() as the second parameter
   // fetch data from the database
-  const [rows] = await connection.execute(query, [cleanCountryCode])
+  const [rows] = await connection.execute(query, [validated])
 
   await connection.end()
   return rows
